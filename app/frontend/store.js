39a0aeec882b1900531c0c180bb8b261137fc5e5
numb86 <86numb@gmail.com>
2019-02-16 11:56:08 +0900

    テストしやすくするために、mutations と actions を単体でも export するようにした。

--- app/frontend/store.js
+++ app/frontend/store.js
@@ -17,6 +17,83 @@ import {
 
 Vue.use(Vuex);
 
+// テストしやすくするために mutations 単体で export している
+export const mutations = {
+  setContentType(state, {path, query}) {
+    const hasSearchKeyword =
+      query[SEARCH_QUERY_KEY] && query[SEARCH_QUERY_KEY].length > 0;
+    if (!hasSearchKeyword) {
+      switch (true) {
+        case path === '/':
+          state.contentType = CONTENT_TYPE_TOP;
+          break;
+        case /^\/tag/.test(path):
+          state.contentType = CONTENT_TYPE_TAG;
+          break;
+        default:
+          state.contentType = null;
+          break;
+      }
+      return;
+    }
+    switch (true) {
+      case path === '/':
+        state.contentType = CONTENT_TYPE_SEARCH;
+        break;
+      case /^\/tag/.test(path):
+        state.contentType = CONTENT_TYPE_TAG_AND_SEARCH;
+        break;
+      default:
+        state.contentType = null;
+        break;
+    }
+  },
+  setBookmarkTotalCount(state, {count}) {
+    state.bookmarkTotalCount = count;
+  },
+  setAllTags(state, {tags}) {
+    state.allTags = tags;
+  },
+  setSearchCondition(state, {path, query}) {
+    const keyword =
+      query[SEARCH_QUERY_KEY] && query[SEARCH_QUERY_KEY].length > 0
+        ? query[SEARCH_QUERY_KEY]
+        : null;
+    const tags = /^\/tag/.test(path) ? path.split('/').slice(2) : [];
+    state.searchCondition = {keyword, tags};
+  },
+  setBookmarkList(state, {bookmarkList}) {
+    state.bookmarkList = bookmarkList;
+  },
+  setPage(state, {query}) {
+    const page =
+      query[PAGE_QUERY_KEY] && query[PAGE_QUERY_KEY].length !== ''
+        ? Number(query[PAGE_QUERY_KEY])
+        : 1;
+    if (page === 0) {
+      state.page = 1;
+      return;
+    }
+    state.page = Number.isNaN(page) ? 1 : page;
+  },
+};
+
+// テストしやすくするために actions 単体で export している
+export const actions = {
+  async fetchBookmarkTotalCount({commit}) {
+    const res = await fetchBookmarkCountApi();
+    commit('setBookmarkTotalCount', {count: res.result});
+  },
+  async fetchAllTags({commit}) {
+    const res = await fetchTagListApi();
+    commit('setAllTags', {tags: res.result});
+  },
+  async fetchBookmarkList({commit}, {searchCondition, page}) {
+    const bookmarkList = await fetchBookmarkListApi(searchCondition, page);
+    commit('setBookmarkList', {bookmarkList});
+  },
+};
+
 export default new Vuex.Store({
   state: {
     contentType: CONTENT_TYPE_TOP,
@@ -37,78 +114,7 @@ export default new Vuex.Store({
     page: 1,
   },
 
-  mutations: {
-    setContentType(state, {path, query}) {
-      const hasSearchKeyword =
-        query[SEARCH_QUERY_KEY] && query[SEARCH_QUERY_KEY].length > 0;
-      if (!hasSearchKeyword) {
-        switch (true) {
-          case path === '/':
-            state.contentType = CONTENT_TYPE_TOP;
-            break;
-          case /^\/tag/.test(path):
-            state.contentType = CONTENT_TYPE_TAG;
-            break;
-          default:
-            state.contentType = null;
-            break;
-        }
-        return;
-      }
-      switch (true) {
-        case path === '/':
-          state.contentType = CONTENT_TYPE_SEARCH;
-          break;
-        case /^\/tag/.test(path):
-          state.contentType = CONTENT_TYPE_TAG_AND_SEARCH;
-          break;
-        default:
-          state.contentType = null;
-          break;
-      }
-    },
-    setBookmarkTotalCount(state, {count}) {
-      state.bookmarkTotalCount = count;
-    },
-    setAllTags(state, {tags}) {
-      state.allTags = tags;
-    },
-    setSearchCondition(state, {path, query}) {
-      const keyword =
-        query[SEARCH_QUERY_KEY] && query[SEARCH_QUERY_KEY].length > 0
-          ? query[SEARCH_QUERY_KEY]
-          : null;
-      const tags = /^\/tag/.test(path) ? path.split('/').slice(2) : [];
-      state.searchCondition = {keyword, tags};
-    },
-    setBookmarkList(state, {bookmarkList}) {
-      state.bookmarkList = bookmarkList;
-    },
-    setPage(state, {query}) {
-      const page =
-        query[PAGE_QUERY_KEY] && query[PAGE_QUERY_KEY].length !== ''
-          ? Number(query[PAGE_QUERY_KEY])
-          : 1;
-      if (page === 0) {
-        state.page = 1;
-        return;
-      }
-      state.page = Number.isNaN(page) ? 1 : page;
-    },
-  },
+  mutations,
 
-  actions: {
-    async fetchBookmarkTotalCount({commit}) {
-      const res = await fetchBookmarkCountApi();
-      commit('setBookmarkTotalCount', {count: res.result});
-    },
-    async fetchAllTags({commit}) {
-      const res = await fetchTagListApi();
-      commit('setAllTags', {tags: res.result});
-    },
-    async fetchBookmarkList({commit}, {searchCondition, page}) {
-      const bookmarkList = await fetchBookmarkListApi(searchCondition, page);
-      commit('setBookmarkList', {bookmarkList});
-    },
-  },
+  actions,
 });
